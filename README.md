b55b3f3f8b30984719794555b19a8ddd0b9fe838
顾重 <gucx@dxy.cn>
2018-03-05 14:38:27 +0800

    feat: init

--- README.md
+++ README.md
@@ -1,2 +1,121 @@
-# proj-gen-git-log
+# Auto Generate Git Log
 自动生成git commit记录用以统计个人项目周报，全组项目周报，版本差异记录等
+
+### Example
+```bash
+# 生成个人周报  ->  xxx.md
+$ ./gen-log.sh
+
+# [2018-02-27]
+#  * feat: sss
+#  * feat: xxx
+#  * chore: xxxsss
+
+# [2018-02-28]
+#  * fix: xxx
+#  * chore：fff
+#  * chore：ggg
+
+# [2018-03-01]
+#  * chore: xxx
+#  * chore: xxx
+#  * feat: xxx
+
+# [2018-03-02]
+#  * fix：sdfdsf
+
+# [2018-03-05]
+#  * feat: xxx
+```
+
+```bash
+# 生成个人周报覆盖已有文件
+$ ./gen-log.sh -f
+```
+
+```bash
+# 生成团队周报  ->  20xx.xx.xx.md
+$ ./gen-log.sh -a ''
+
+# [2018-02-26]
+#  * refactor: sdfa (dachui)
+#  * fix: 1639 (顾重)
+#  * feat: gggg (dachui)
+#  * feat: aaaaa (dachui)
+
+# [2018-02-27]
+#  * chore: sdf (dachui)
+#  * chore: 代码格式化 (dachui)
+#  * chore: sdf (顾重)
+
+# [2018-02-28]
+#  * fix: 1111 (顾重)
+#  * chore: 22222 (dachui)
+
+# [2018-03-01]
+#  * chore: vvvv (顾重)
+#  * chore: ssss (顾重)
+#  * feat: bbbb (顾重)
+#  * fix: asdf (dachui)
+
+# [2018-03-05]
+#  * feat: 2222 (顾重)
+```
+
+```bash
+# 生成待发布版本信息 ->  vx.x.x.md
+$ ./gen-log.sh -a '' -t master -o develop -s 1970-01-01
+
+# > 2018-03-05
+
+# #### 新增
+# * sdf (lizhen)
+# * 234 (dachui)
+# * hh (dachui)
+# * bbb (dachui)
+
+# #### 修改
+# * 2222 (asdf)
+
+# #### 重构
+# * vvvv (dachui)
+
+# #### 文档
+# * aaaa (dachui)
+
+# #### 其他
+# * ssss&优化代码 (lizhen)
+# * vvvv (dachui)
+# * 123 (顾重)
+```
+
+```bash
+# 生成目录为version
+$ ./gen-log.sh -d version
+```
+
+### Options
+* -a	贡献者；默认：git 全局配置 name；可传 '' 表示所有贡献者
+* -s	起始时间；默认：上周一
+* -e	终止时间；默认：当天
+* -o	设置比对分支源分支名；默认当前分支
+* -r	本地项目路径；默认当前目录
+* -t	设置比对分支目标分支名；默认当前分支
+* -v	设置比对版本；默认package.json中version字段值
+* -f	设置是否强制覆盖已有文件；默认：不覆盖
+* -d	设置输出目录；默认：log
+
+### Usage
+ * 命令行进入项目根目录
+ * 执行`./gen-log.sh`
+ * 自动在`log`文件夹(若无会自动创建)下生成{user}.md文件
+
+### Caution
+ * 每个版本需**修改`package.json`版本号**
+ * 尽可能**保证功能分支commit message简明扼要并且没有无意义commit**(建议提测前rebase，可参考[Git常规操作](https://note.youdao.com/share/?id=b423064d79fb2f975165d3fc5a79ae5d&type=note#/))
+ * 提交MR将`develop`合入`master`**前**执行命令
+ * 代码合并后新增tag，复制粘贴log文件内容即可
+
+### Other
+* 可直接执行`.sh`文件，报错提示无权限需要执行`chmod 755 ./gen-log.sh`
+* 根据配置项可生成各种git记录，欢迎优化拓展
---
+++ gen-log.sh
@@ -0,0 +1,224 @@
+
+#!/bin/bash
+
+# Generates git changelog
+#
+# optional parameters
+# -a	to filter by author
+# -s	to select start date
+# -e	to select end date
+# -o	to select origin branch
+# -r	to specify the repository path
+# -t	to select target branch
+# -v	to set version number
+# -f	to set fouce gen file
+# -d	to set output dir
+
+# 作者，默认git全局配置name
+AUTHOR=$(git config user.name)
+# 起始时间，默认上周一
+SINCE="last.Monday"
+# 终止时间，默认当天
+UNTIL=$(date +%F)
+# 格式化
+LOG_FORMAT=" %Cgreen*%Creset %s"
+# 输出目录
+OUTPUT_DIR="log"
+# commit比对，源分支，当前分支
+ORIGIN="$(git rev-parse --abbrev-ref HEAD)"
+# commit比对，目标分支，默认当前分支
+TARGET=$ORIGIN
+# commit 类型 键
+TYPE_MAP=(feat fix refactor style docs chore)
+# commit 类型 值
+TYPE_TITLE_MAP=(新增 修改 重构 样式 文档 其他)
+# 强制生成文件
+FOUCE=0
+# 是否打印时间
+PRINT_TIME=1
+
+# function，去除字符串两头空格
+trim()
+{
+  trimmed=$1
+  trimmed=${trimmed%% }
+  trimmed=${trimmed## }
+  
+  echo $trimmed
+}
+
+# 传参覆盖默认值
+while getopts "a:s:e:o:d:r:t:v:f" arg
+do
+  case $arg in
+    a)
+      AUTHOR=$OPTARG
+    ;;
+    s)
+      SINCE=$OPTARG
+    ;;
+    e)
+      UNTIL=$OPTARG
+    ;;
+    o)
+      ORIGIN=$OPTARG
+    ;;
+    r)
+      REPO=$OPTARG
+    ;;
+    t)
+      TARGET=$OPTARG
+    ;;
+    v)
+      VERSION=$OPTARG
+    ;;
+    f)
+      FOUCE=1
+    ;;
+    d)
+      OUTPUT_DIR=$OPTARG
+    ;;
+    ?)
+      echo "unknown argument"
+      exit 1
+    ;;
+  esac
+done
+
+# 判断作者
+if [ -z $AUTHOR ]
+then
+  # 作者为空
+  LOG_FORMAT="$LOG_FORMAT %Cblue(%an)%Creset"
+  
+  # 判断输出文件配置
+  if [ -z $OUTPUT ]
+  then
+    # 输出文件路径，默认“当前日期.md”
+    OUTPUT="${OUTPUT_DIR}/$(date +%F).md"
+  fi
+else
+  # 输出文件路径，默认“作者名.md”
+  OUTPUT="${OUTPUT_DIR}/${AUTHOR}.md"
+fi
+
+# 判断是否使用commit比对模式
+if [ $ORIGIN != $TARGET ]
+then
+  DIFF="$TARGET..$ORIGIN"
+  
+  # 抓取版本
+  while read line
+  do
+    # 抓取定义version行文本
+    if [[ ${line} = *"version"* ]]
+    then
+      # 移除双引号
+      VERSION=${line//\"/ }
+      # 移除键名
+      VERSION=${VERSION##*"version :"}
+      # 获取版本
+      VERSION=${VERSION%%" ,"}
+      VERSION=$(trim $VERSION)
+      break
+    fi
+  done < package.json
+  
+  # 输出文件路径，默认“v版本.md”
+  OUTPUT="${OUTPUT_DIR}/v${VERSION}.md"
+fi
+
+# 指定目录但不存在则创建
+if [ ! -z $OUTPUT_DIR -a ! -e $OUTPUT_DIR ]
+then
+  mkdir $OUTPUT_DIR
+fi
+
+# 检查文输出件存在 & 不强制生成 则退出
+if [ -e $OUTPUT -a $FOUCE -eq 0 ]
+then
+  echo "${OUTPUT} already exists! \nPlease update package.json or remove log file!\n"
+  exit 1
+fi
+
+(
+  git -C "${REPO}" log $DIFF --author="${AUTHOR}" --since="${SINCE}" --until="${UNTIL}" --format="%cd" --date=short | sort -u | while read DATE ; do
+    GIT_PAGER=$(git -C "${REPO}" log $DIFF --no-merges --reverse --format="${LOG_FORMAT}" --since="${DATE} 00:00:00" --until="${DATE} 23:59:59" --author="${AUTHOR}")
+    if [ ! -z "$GIT_PAGER" ]
+    then
+      if [ ! -z $DIFF ]
+      then
+        # commit比对模式
+        
+        # 字符串分隔符
+        IFS="*"
+        # 分割字符串为数组
+        arr=($GIT_PAGER)
+        # 还原分割符，否则会导致if判断失效
+        IFS=""
+        # 循环处理数组
+        for s in ${arr[@]}
+        do
+          # 去除字符串两头空格
+          s=$(trim $s)
+          # 判断字符串非空
+          if [ ! -z $s ]
+          then
+            # 替换全角冒号
+            s=${s/：/:}
+            # 循环commit 类型
+            for type in ${TYPE_MAP[@]}
+            do
+              # 组织正则
+              reg="${type}:"
+              # 判断commit类型
+              if [[ ${s} = *"${reg}"* ]]
+              then
+                # 裁剪字符串
+                s=${s##*${reg}}
+                # 移除空格
+                s=$(trim $s)
+                # 动态数组变量赋值
+                eval COMMIT_${type}='(${COMMIT_'${type}'[*]} $s)'
+                break
+              fi
+            done
+          fi
+        done
+        
+        if [ $PRINT_TIME -eq 1 ]
+        then
+          echo "> ${UNTIL}\n"
+        fi
+        
+        # 处理数据
+        typeIndex=0
+        for type in ${TYPE_MAP[@]}
+        do
+          # 拷贝数组
+          eval type='(${COMMIT_'${type}'[*]})'
+          
+          if [ ${#type[*]} != 0 ]
+          then
+            echo "#### ${TYPE_TITLE_MAP[$typeIndex]}"
+            
+            for i in ${type[@]}
+            do
+              echo "* ${i}"
+            done
+            echo
+          fi
+          let typeIndex++
+        done
+      else
+        # 普通模式
+        echo "[${DATE}]"
+        echo "${GIT_PAGER}"
+        echo
+      fi
+    fi
+  done
+) > $OUTPUT
+# | less -R
+
+echo "log is written to ${OUTPUT}"
