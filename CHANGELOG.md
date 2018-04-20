3af39b209802737bec5089140f35e87ddca43971
guchongxi <gucx@dxy.cn>
2018-04-21 01:29:49 +0800

    feat: 增加changelog模式

---
+++ CHANGELOG.md
@@ -0,0 +1,32 @@
+## [v1.1.1](http://github.com/guchongxi/gen-git-log/compare/v1.0.2...v1.1.1) (2018-04-21)
+#### 新增
+* 增加tag对比 ([b1e5a67](http://github.com/guchongxi/gen-git-log/commit/b1e5a67b4422b8261cea4ac0c7c750686afeb763))  @gucx
+* 增加changelog模式 ([fc4a5e7](http://github.com/guchongxi/gen-git-log/commit/fc4a5e724389b055ea7b5fa7dc23d98b8d360d07))  @gucx
+
+#### 修改
+* 修复无法自定义版本问题 ([b1f93b9](http://github.com/guchongxi/gen-git-log/commit/b1f93b9cfdbd246b93b09c64f040169cbd06751e))  @gucx
+* 获取默认版本时应读取设定仓库tag ([bd4a952](http://github.com/guchongxi/gen-git-log/commit/bd4a9521286c15d00d927bb097beb1aec01089fd))  @gucx
+* 生成tag差异标题中source链接错误 ([9bb22c0](http://github.com/guchongxi/gen-git-log/commit/9bb22c0d4ad824967261380bc8e557401158ae48))  @gucx
+
+#### 文档
+* 更新readme ([4d7465a](http://github.com/guchongxi/gen-git-log/commit/4d7465afca637eb76ad74c7963eff46048434e79))  @gucx
+* 更新README使用方法 ([0131beb](http://github.com/guchongxi/gen-git-log/commit/0131beb31873b63376f0c6358a7d0091bdd9bf03))  @gucx
+
+#### 其他
+* 更新至1.1.0 ([3e26cbc](http://github.com/guchongxi/gen-git-log/commit/3e26cbc840844684456a8fd941cd397153d65cba))  @gucx
+* 增加关键词 ([0e7d770](http://github.com/guchongxi/gen-git-log/commit/0e7d77067d8ab7d457a5c5d4f032b14cfabe83bb))  @gucx
+## [v1.0.2](http://github.com/guchongxi/gen-git-log/compare/v1.0.0...v1.0.2) (2018-03-08)
+#### 新增
+* 修改输出格式，增加commit远程链接 ([5266bc4](http://github.com/guchongxi/gen-git-log/commit/5266bc430fc0a5b0198f9e3ea299b4a42c52e6db))  @gucx
+
+#### 其他
+* 准备发布 ([64bac42](http://github.com/guchongxi/gen-git-log/commit/64bac4235589ef1a465c99a7a742b245f5ba14a8))  @gucx
+* 更新命令，readme ([b22c150](http://github.com/guchongxi/gen-git-log/commit/b22c150f23515e2c699cd5ea2f0905d4d73bb141))  @gucx
+* update readme ([91d4256](http://github.com/guchongxi/gen-git-log/commit/91d4256467fac1ca3df2f69aa7306bbd876889bb))  @gucx
+## [v1.0.0](http://github.com/guchongxi/gen-git-log/compare/edf3a1...v1.0.0) (2018-03-05)
+#### 新增
+* init ([b55b3f3](http://github.com/guchongxi/gen-git-log/commit/b55b3f3f8b30984719794555b19a8ddd0b9fe838))  @gucx
+
+#### 修改
+* 修复生成版本记录时重复问题 ([94e0f86](http://github.com/guchongxi/gen-git-log/commit/94e0f8696c3b026d1c183c2f9d863de25f286f81))  @gucx
+
--- gen-log.sh
+++ gen-log.sh
@@ -35,6 +35,7 @@ trim() {
   trimmed=$1
   trimmed=${trimmed%% }
   trimmed=${trimmed## }
+  trimmed=${trimmed## }
 
   echo $trimmed
 }
@@ -98,18 +99,98 @@ getVersion() {
         VERSION=${VERSION##*"version :"}
         # 获取版本
         VERSION=${VERSION%%" ,"}
-        VERSION=$(trim $VERSION)
         break
       fi
     done < $PKG_PATH
   fi
-
-  echo $VERSION
+  trim $VERSION
 }
 # function，生成输出文件路径
 generateOutPutPath() {
   # 输出文件路径，默认“v版本.md”
   echo "${OUTPUT_DIR}/v$(getVersion).md"
+}
+# 生成指定SOURCE TARGET tag差异记录
+genSingleTagLog() {
+    SOU=$1
+    TAR=$2
+
+    if [ ! "$3"x = "0"x ]
+    then
+      if [ $SOU = HEAD ]
+      then
+        # 如果是与最新HEAD对比则将HEAD设为version
+        TIT="## [v$(getVersion)](http://${REMOTE}/compare/${TAR}...v$(getVersion))"
+      else
+        TIT="## [${SOU}](http://${REMOTE}/compare/${TAR}...${SOU})"
+      fi
+
+      echo "${TIT} ($(git -C "${REPO}" log -1 --format=%ad --date=short $SOU))"
+    fi
+
+    # 直接使用分支比对查找所有匹配 log
+    GIT_PAGER=$(git -C "${REPO}" log --no-merges --reverse --format="${LOG_FORMAT}" "$SOU...$TAR")
+
+    if [ ! -z "$GIT_PAGER" ]
+    then
+      # 字符串分隔符
+      IFS="*"
+      # 分割字符串为数组
+      singleTagArr=($GIT_PAGER)
+      # 还原分割符，否则会导致if判断失效
+      IFS=""
+      # 循环处理数组
+      for s in ${singleTagArr[@]}
+      do
+        # 去除字符串两头空格
+        s=$(trim $s)
+        # 判断字符串非空
+        if [ ! -z $s ]
+        then
+          # 替换全角冒号
+          s=${s/：/:}
+          # 循环commit 类型
+          for type in ${TYPE_MAP[@]}
+          do
+            # 组织正则
+            reg="${type}:"
+            # 判断commit类型
+            if [[ ${s} = *"${reg}"* ]]
+            then
+              # 裁剪字符串
+              s=${s##*${reg}}
+              s=${s%@*}
+              # 移除空格
+              s=$(trim $s)
+              # 动态数组变量赋值
+              eval COMMIT_${type}='(${COMMIT_'${type}'[*]} $s)'
+              break
+            fi
+          done
+        fi
+      done
+
+      # 处理数据
+      typeIndex=0
+      for type in ${TYPE_MAP[@]}
+      do
+        # 拷贝数组
+        eval type='(${COMMIT_'${type}'[*]})'
+
+        # 判断数组是否含有元素
+        if [ ${#type[*]} != 0 ]
+        then
+          echo "#### ${TYPE_TITLE_MAP[$typeIndex]}"
+
+          for i in ${type[@]}
+          do
+            echo "* ${i}"
+          done
+          echo
+        fi
+        let typeIndex++
+      done
+    fi
 }
 
 # 传参覆盖默认值
@@ -166,7 +247,7 @@ do
     git-log [options]\n
 
   Options:\n
-    -m  生成模式  默认：无(周报)，可选：branch(分支比对)、tag(标签比对)
+    -m  生成模式  默认：无(周报)，可选：branch(分支比对)、tag(标签比对)、changelog(汇总日志)
     -a  想要过滤的作者  默认：$(git config user.name)
     -s  起始日期  默认：上周一，格式：2018-01-01
     -u  终止日期  默认：当天，格式：2018-01-01
@@ -344,74 +425,58 @@ case $MODE in
       TARGET=$LASTEST_TAG
     fi
 
-    # 直接使用分支比对查找所有匹配 log
-    GIT_PAGER=$(git -C "${REPO}" log "$SOURCE...$TARGET" --no-merges --reverse --format="${LOG_FORMAT}")
 
     (
       printTimeIfNeed
+      genSingleTagLog $SOURCE $TARGET 0
+    ) > $OUTPUT
+  ;;
+  changelog)
+    if [ -z $REPO ]
+    then
+      OUTPUT="./CHANGELOG.md"
+    else
+      OUTPUT="${REPO}/CHANGELOG.md"
+    fi
 
-      if [ ! -z "$GIT_PAGER" ]
-      then
-        # 字符串分隔符
-        IFS="*"
-        # 分割字符串为数组
-        arr=($GIT_PAGER)
-        # 还原分割符，否则会导致if判断失效
-        IFS=""
-        # 循环处理数组
-        for s in ${arr[@]}
-        do
-          # 去除字符串两头空格
-          s=$(trim $s)
-          # 判断字符串非空
-          if [ ! -z $s ]
-          then
-            # 替换全角冒号
-            s=${s/：/:}
-            # 循环commit 类型
-            for type in ${TYPE_MAP[@]}
-            do
-              # 组织正则
-              reg="${type}:"
-              # 判断commit类型
-              if [[ ${s} = *"${reg}"* ]]
-              then
-                # 裁剪字符串
-                s=${s##*${reg}}
-                s=${s%@*}
-                # 移除空格
-                s=$(trim $s)
-                # 动态数组变量赋值
-                eval COMMIT_${type}='(${COMMIT_'${type}'[*]} $s)'
-                break
-              fi
-            done
-          fi
-        done
+    LOG_FORMAT="$FORMAT_DEFAULT @%ae"
 
-        # 处理数据
-        typeIndex=0
-        for type in ${TYPE_MAP[@]}
-        do
-          # 拷贝数组
-          eval type='(${COMMIT_'${type}'[*]})'
+    shouldFouceResolve
 
-          # 判断数组是否含有元素
-          if [ ${#type[*]} != 0 ]
-          then
-            echo "#### ${TYPE_TITLE_MAP[$typeIndex]}"
+    # 标签列表
+    TAG_LIST=$(git -C "${REPO}" tag)
 
-            for i in ${type[@]}
-            do
-              echo "* ${i}"
-            done
-            echo
-          fi
-          let typeIndex++
-        done
-      else
-        echo "${TARGET}...${SOURCE} 版本无差异"
-      fi
+    # 字符串分隔符
+    IFS="v"
+    # 分割字符串为数组
+    arr=($TAG_LIST)
+    # 还原分割符，否则会导致if判断失效
+    IFS=""
+    # 第一次提交commit, 取duan hash
+    LAST_TAG=$(git -C "${REPO}" rev-list --reverse HEAD | head -1)
+    LAST_TAG=${LAST_TAG:0:6}
+    FIRST_TAG="HEAD"
+
+    len=$[${#arr[@]}-1]
+
+    (
+      # 循环处理数组
+      for ((i=$len;i>0;i--))
+      do
+        s=${arr[$i]}
+        # 去除字符串两头空格
+        s=v$(trim $s)
+        # 判断字符串非空
+        if [ ! -z $s ]
+        then
+          # 不知道为什么一定要缓存一下，否则$s值错乱
+          TEMP=$s
+          echo $(genSingleTagLog $FIRST_TAG $s)
+          FIRST_TAG=$TEMP
+        fi
+      done
+
+      genSingleTagLog $FIRST_TAG $LAST_TAG
     ) > $OUTPUT
   ;;
   *)
